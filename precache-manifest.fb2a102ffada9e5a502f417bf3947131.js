5a50e7e802525cf08052f2d2ca29d8baa43c74ce
sydoroknadiia <sydorok.nadiia@gmail.com>
2020-02-02 23:16:02 +0200

    Updates

---
+++ precache-manifest.fb2a102ffada9e5a502f417bf3947131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50eb3edf9dcfd1ce8957459f06daf329",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6b619ee7dd0c90d35d61",
+    "url": "/goit-react-hw-01-components/static/css/main.82a9e1b2.chunk.css"
+  },
+  {
+    "revision": "1224a5e0e7d8ff04f4dc",
+    "url": "/goit-react-hw-01-components/static/js/2.25b66531.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-01-components/static/js/2.25b66531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b619ee7dd0c90d35d61",
+    "url": "/goit-react-hw-01-components/static/js/main.2b603de0.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
